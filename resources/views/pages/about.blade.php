9ac149584e1d48d0089a91d2f9721e2997d8ba23
dinaOuahbi <dina_ouahbi@yahoo.com>
2023-03-11 16:54:06 +0100

    Remove duplications by using app layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,20 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
 
-        <title>About Us | Hello city</title>
-    </head>
-    <body>
-        <p>Built with &hearts; by Dina OUAHBI</p>
+@section('content')
 
-        <p><a href="/">Go back to the home page</a></p>
+<p>Built with &hearts; by Dina OUAHBI</p>
+<p><a href="/">Go back to the home page</a></p>
 
-        <footer>
-            <p>&copy; Copyright {{date('Y')}} &middot;</p>
-        </footer>
-
-        
-    </body>
-</html>
+@endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,22 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
 
-        <title>Hello city</title>
-    </head>
-    <body>
-        <h1>HELLO FROM DIJON </h1>
+@section('content')
 
-        <p>it's currently {{date("F j, Y, g:i a")}}</p>
+<h1>HELLO FROM DIJON </h1>
+<p>it's currently {{date("F j, Y, g:i a")}}</p>
 
-        <footer>
-            <p>
-                &copy; Copyright {{date('Y')}} &middot; <a href="/about-us">About Us</a>
-            </p>
-        </footer>
-
-        
-    </body>
-</html>
+@endsection
